1f94a6146299974e193b04a2969cecc6f65a46da
Anastasia Nevesenko <nnnevesenko@gmail.com>
2023-10-27 16:31:10 +0300

    Initial commit

---
+++ movable_objects.py
@@ -0,0 +1,11 @@
+import pygame
+import pymunk
+
+
+class Lover:
+
+    def _add_image(self, g: object) -> object:
+        return
+
+    def __init__(self, gender: bool):
+        self.pic = self._add_image(gender)
