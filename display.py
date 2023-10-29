0706501d32d8b4f333798f6f0c47da5c543ce417
Anastasia Nevesenko <nnnevesenko@gmail.com>
2023-10-30 00:04:39 +0300

    added characters and their pictures added new method for moving added updating system

--- display.py
+++ display.py
@@ -1,4 +1,35 @@
-import pygame
-import pymunk
+import pygame as pg
+import pymunk.pygame_util
+import movable_objects as mo
 
 
+if __name__ == '__main__':
+    RES = WIDTH, HEIGHT = 800, 620
+    FPS = 60
+    pg.init()
+    surface = pg.display.set_mode(RES)
+    clock = pg.time.Clock()
+    draw_options = pymunk.pygame_util.DrawOptions(surface)
+    # настройки pymunk
+    space = pymunk.Space()
+    space.gravity = 0, 8000
+
+    man = mo.Lover(mo.Gender.M, space, surface)
+    woman = mo.Lover(mo.Gender.W, space, surface)
+
+
+    while True:
+       # surface.fill(pg.Color('black'))
+        for i in pg.event.get():
+            if i.type == pg.QUIT:
+                exit()
+            if i.type == pg.MOUSEBUTTONDOWN:
+                if i.button == 1:
+                    pass
+        surface.fill(pg.Color('black'))
+        for char in (man, woman):
+           char.change_pos(surface)
+        mo.check_distance(man, woman)
+
+        pg.display.flip()
+        clock.tick(FPS)
--- movable_objects.py
+++ movable_objects.py
@@ -1,11 +1,47 @@
-import pygame
+import pygame as pg
 import pymunk
+from enum import Enum
+import asyncio
 
+
+
+
+class Gender(Enum):
+    M = 1
+    W = 2
 
 class Lover:
+    def _add_image(self, g :Gender) -> object:
+        if g == Gender.M:
+            path = 'Images/lollypop.png'
+            SIZE = (80,120)
+        elif g == Gender.W:
+            path = 'Images/donut.png'
+            SIZE = (85,85)
+        else:
+            return ValueError
+        self.image = pg.image.load(path)
 
-    def _add_image(self, g: object) -> object:
-        return
-
-    def __init__(self, gender: bool):
+        return pg.transform.scale(self.image,SIZE)
+    def __init__(self, gender: Gender, space, surface):
+        self.obj = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
+        self.delta = 0.5
+        if gender == Gender.M:
+            self.pos = [600,100]
+            self.delta = -self.delta
+        else:
+            self.pos = [100,100]
         self.pic = self._add_image(gender)
+        space.add(self.obj)
+        surface.blit(self.pic, (self.pos))
+
+    def change_pos(self, surface):
+        self.pos[0] += self.delta
+        self.obj.position = self.pos
+        surface.blit(self.pic, (self.pos))
+
+def check_distance(x1: Lover, x2: Lover):
+    distance = abs(x1.obj.position[0] - x2.obj.position[0])
+    print(distance)
+    if distance < 4:
+        pass
